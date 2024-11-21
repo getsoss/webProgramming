2b179c08264cf59b3bf597752931cdf461962f5d
getsoss <getsoss@naver.com>
2024-11-21 19:13:37 +0900

    first commit

---
+++ src/test/java/com/example/webProgramming/WebProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webProgramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
